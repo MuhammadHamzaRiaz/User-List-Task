90306e46ea36de7804239a5bade5baa69d58b5ac
Hamza Riaz <hamza.riaz1431@gmail.com>
2022-10-31 23:50:20 +0500

    almost done

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,48 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from 'axios';
+
 
 export default createStore({
   state: {
+    userStories: [],
   },
   getters: {
+    userStories: (state) => state.userStories,
   },
   mutations: {
+    async setStory(state, story) {
+      await axios.post('https://user-stories-4a4ce-default-rtdb.firebaseio.com/' , story)
+      state.userStories.push(story);
+      localStorage.setItem("userStories", JSON.stringify(state.userStories));
+    },
+    dltStory(state, story) {
+      state.userStories.splice(story , 1)
+      localStorage.setItem("userStories", JSON.stringify(state.userStories));
+    },
+    getStory( state , stories) {
+    state.userStories = stories;
+    },
+    flipStoryId(state, id){
+      state.userStories[id].completed = !state.userStories[id].completed;
+      localStorage.setItem("userStories", JSON.stringify(state.userStories));
+
+    }
   },
   actions: {
+    addStory({ commit }, story) {
+      commit("setStory", story);
+    },
+    deleteStory({ commit }, story) {
+
+      commit("dltStory", story);
+    },
+    getStories({ commit }) {
+      const stories = JSON.parse(localStorage.getItem("userStories")) || [];
+      commit("getStory" , stories)
+    },
+    flipStory({commit}, id){
+      commit("flipStoryId", id)
+    }
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,60 @@
 <template>
-  <div class="text-center !max-w-sm pt-5">
-    <h1 class="text-2xl">
+  <div class="text-center w-80 mx-auto  pt-5">
+    <h1 class="text-2xl py-5">
       User Todo List
     </h1>
     <div>
-      <v-text-field  :rules="rules"></v-text-field>
+      <v-text-field v-model="userStory" @keydown.enter.prevent="sentStory"></v-text-field>
+    </div>
+    <div class=" bg-blue-100 p-5 rounded-xl">
+      <v-list lines="one" class="!bg-transparent">
+        <v-list-item v-for="(item, idx ) in userStories" :key="idx" class="capitalize text-black"
+          rounded="shaped">
+          <template v-slot:prepend>
+            <v-icon icon="mdi-calendar-check-outline"></v-icon>
+          </template>
+          {{ item.text }}
+          <template v-slot:append>
+            <v-icon v-if="item.completed" color="secondary" icon="mdi-check-all"
+              @click.prevent="flipStory(idx)"></v-icon>
+            <v-icon v-else icon="mdi-check" @click.prevent="flipStory(idx)"></v-icon>
+            <v-icon icon="mdi-delete" class="text-red-700 !opacity-100" @click="deleteItem(idx)"></v-icon>
+          </template>
+        </v-list-item>
+      </v-list>
     </div>
   </div>
 </template>
-
 <script>
+
 export default {
   data: () => ({
-    rules: [
-      value => !!value || 'Required.',
-    ],
+    userStory: "",
   }),
+  computed: {
+    userStories() {
+      return this.$store.getters.userStories;
+    }
+
+  },
+  methods: {
+    sentStory() {
+      if (this.userStory) {
+        this.$store.dispatch("addStory", { completed: false, text: this.userStory })
+        this.userStory = ""
+      }
+    },
+    deleteItem(id) {
+      this.$store.dispatch("deleteStory", id);
+    },
+    flipStory(id) {
+      this.$store.dispatch("flipStory", id)
+    }
+
+  },
+  mounted() {
+    this.$store.dispatch("getStories");
+  },
+
 }
 </script>
